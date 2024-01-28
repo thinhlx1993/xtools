3f31479995bc397df959544c05749773d7477e10
thinh <thinhle.ict@gmail.com>
2024-01-28 18:44:13 +0700

    revert host

--- src/renderer/src/config/enums.jsx
+++ src/renderer/src/config/enums.jsx
@@ -1,6 +1,6 @@
 const AppConfig = {
-  BASE_URL: 'http://localhost:8080/api/v1',
-  // BASE_URL: 'http://157.230.192.238/api/v1',
+  // BASE_URL: 'http://localhost:8080/api/v1',
+  BASE_URL: 'http://157.230.192.238/api/v1',
   ADMIN_ROLE_ID: 'b40ee1ae-5a12-487a-98cc-b6d07238e17a',
   CLIENT_ROLE_ID: '270325cc-0378-48f2-8b18-67e1c22a64c5',
   HMA_BASE_URL: 'https://api-tuan.hidemyacc.com'
