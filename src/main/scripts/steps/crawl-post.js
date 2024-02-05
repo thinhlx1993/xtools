14384046c8b41950994c82dd3c82b6a86ede2232
thinh <thinhle.ict@gmail.com>
2024-02-06 00:01:52 +0700

    remove comment

--- src/main/scripts/steps/crawl-post.js
+++ src/main/scripts/steps/crawl-post.js
@@ -1,12 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import puppeteer from 'puppeteer-core'
-import { authenticateProxy, testRegHttpEvent } from '../helpers'
+import { testRegHttpEvent } from '../helpers'
 import { PAGE_URL } from '../constants'
 import logger from '../../logger'
 import { regGraphAPIUserTws } from '../regex'
 import utils from '../utils'
 import mapper from '../mapper'
-import repository from '../../database/repository'
 import { getProfileData, getLastPostSaved, createAnewPost } from '../services/backend'
 const dataMemories = {}
 
@@ -42,17 +41,6 @@ const _filterAndCache = async (profileCrawl, profileData, response) => {
         if (!reMapped || reMapped.isAds || reMapped.isRePost) {
           return
         }
-        //  entry: JSON.stringify(entry),
-        // return {
-        //   profile_id: profileData.profile_id,
-        //   username: profileCrawl,
-        //   tw_post_id: entry.entryId,
-        //   like: entry.content.itemContent.tweet_results.result.legacy.favorite_count,
-        //   comment: entry.content.itemContent.tweet_results.result.legacy.reply_count,
-        //   share: entry.content.itemContent.tweet_results.result.legacy.retweet_count,
-        //   content: JSON.stringify(entry),
-        //   post_date: entry.content.itemContent.tweet_results.result.legacy.created_at
-        // }
         return {
           profile_id: profileData.profile_id,
           username: profileCrawl,
@@ -66,8 +54,6 @@ const _filterAndCache = async (profileCrawl, profileData, response) => {
       })
       .filter(Boolean)
     if (entriesSave.length) {
-      // logger.info(`entriesSave ${JSON.stringify(entriesSave)}`)
-      // await repository.savePosts(entriesSave)
       for (const post of entriesSave) {
         await createAnewPost(post)
       }
@@ -92,7 +78,6 @@ const _base = async (page, profileData, profileCrawl) => {
       (response) => testRegHttpEvent(regGraphAPIUserTws, response) && response.status() === 200
     )
     await _filterAndCache(profileCrawl, profileData, await postResponse.json())
-    // await _filterAndCache(profileId, accountId, await postResponse.json())
   } catch (error) {
     logger.error('CRAWL_POST_ERROR', error)
   }
